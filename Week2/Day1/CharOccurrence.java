77318acdeaa45a599ef09a13bc2a2073a453473f
JananiSantharaman <103834158+JananiSantharaman@users.noreply.github.com>
2022-04-23 15:00:09 +0530

    Add files via upload

---
+++ Week2/Day1/CharOccurrence.java
@@ -0,0 +1,29 @@
+package week2.day1;
+
+public class CharOccurrence {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//scenario 1
+		//srting "testttleaf" 
+		//print the numer of e in the string
+		String test0 = "Janani";
+		//System.out.println(test0.length());
+		int outputval = 0;
+		int compareval = 'f';
+		for (int i =0;i<test0.length();i++) {	
+			char  value1 = test0.charAt(i);
+			if ( value1==compareval) {
+				outputval =outputval +1 ;
+			}
+		}
+		//Compare if the char is present in the given string
+		if(outputval!=0) {
+			System.out.println("The number of occurence of the charecter is = "+ outputval);
+		}
+		else {
+			System.out.println("The Character is not found in the given string");
+		}
+	}
+
+}
