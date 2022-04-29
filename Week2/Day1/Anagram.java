61d4c836c6aefb5e6729e1ebb27d862e866fa987
JananiSantharaman <103834158+JananiSantharaman@users.noreply.github.com>
2022-04-29 12:38:26 +0530

    Update Anagram.java

--- Week2/Day1/Anagram.java
+++ Week2/Day1/Anagram.java
@@ -1,14 +1,11 @@
 package week2.day1;
-
 import java.util.Arrays;
-
 public class Anagram {
 	public static void main (String[] args) {
 		//Declare a String 
 		String text1 = "stops";		
 		//Declare another String
 		String text2 = "potss"; 
-
 		// a) Check length of the strings are same then (Use A Condition)
 		if(text1.length()==text2.length()) {	
 			//b) Convert both Strings in to characters and sort them
@@ -16,7 +13,6 @@ public class Anagram {
 			Arrays.sort(arr1);
 			char[] arr2 = text2.toCharArray();
 			Arrays.sort(arr2);		
-
 			// d) Check both the arrays has same value
 			for (int i = 0; i < arr1.length; i++) {
 				//System.out.println("Array1 "+arr1[i]);
