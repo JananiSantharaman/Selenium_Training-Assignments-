0707c3e3ae4c504ee16d2faf45b49ac5b9ca6448
JananiSantharaman <103834158+JananiSantharaman@users.noreply.github.com>
2022-05-01 09:42:59 +0530

    Add files via upload

---
+++ Week3/day1/Override/AxisBank.java
@@ -0,0 +1,13 @@
+package week3.day1;
+
+public class AxisBank extends BankInfo{
+public void deposit() {
+	System.out.println("AxisBank class deposit");
+}
+
+public  static void main(String[] args) {
+	//AxisBank depositoverride = new AxisBank();
+	//depositoverride.deposit();
+int x =10*20-20;System.out.println(x);
+}
+}
