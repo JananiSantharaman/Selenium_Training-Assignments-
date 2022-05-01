190a8beb6dd96e86c1ccaeff73a5222f3a2ff5eb
JananiSantharaman <103834158+JananiSantharaman@users.noreply.github.com>
2022-05-01 09:42:33 +0530

    Create BankInfo.java

---
+++ Week3/day1/Override/BankInfo.java
@@ -0,0 +1,14 @@
+package week3.day1;
+
+public class BankInfo {
+
+	public  void fixed() {
+		System.out.println("BankInfo class fixed");
+	}
+	public  void saving() {
+		System.out.println("BankInfo class saving");
+	}	
+	public  void deposit() {
+		System.out.println("BankInfo class deposit");
+	}
+}
