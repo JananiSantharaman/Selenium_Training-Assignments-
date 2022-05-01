c139771b19e068acd4976140df75caf6fe3525e9
JananiSantharaman <103834158+JananiSantharaman@users.noreply.github.com>
2022-05-01 09:41:06 +0530

    Add files via upload

---
+++ Week3/day1/MultipleInterference/org.student/College.java
@@ -0,0 +1,13 @@
+package org.college;
+
+public class College {
+	public void collegeName() {
+		System.out.println("Class College - Method - College name");
+	}
+	public void collegeCode() {
+		System.out.println("Class College - Method - College Code");
+	}
+	public void collegeRank() {
+		System.out.println("Class College - Method - College collegeRank");
+	}
+}
