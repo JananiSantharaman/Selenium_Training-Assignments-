0b42d057955efb6e58622d304fcc8e53f517681f
JananiSantharaman <103834158+JananiSantharaman@users.noreply.github.com>
2022-05-09 18:46:15 +0530

    Add files via upload

---
+++ week4/day1/TableCountAssignment.java
@@ -0,0 +1,43 @@
+package week4.day1;
+
+import java.time.Duration;
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class TableCountAssignment {
+
+	public static void main(String[] args) {
+		//set up the driver and create ChromeDriver Instance 	
+		WebDriverManager.chromedriver().setup();
+		ChromeDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+		//Load the URL: https://html.com/tags/table/
+		driver.get("https://html.com/tags/table/");
+		//get the table count in the page
+		int tablecount = driver.findElements(By.xpath("(//table)")).size();
+		System.out.println("Total number of tables in the page is - "+tablecount);
+		System.out.println("-------------------------------------------------");
+		for(int i= 1;i<=tablecount;i++) {
+		//get the row count of table
+		int table1rowcount =driver.findElements(By.xpath("(//table)["+i+"]//tr")).size();
+		System.out.println("The total rows in the table in table "+i+" is - "+table1rowcount);
+		//get the column count of table
+		int table1colcount =driver.findElements(By.xpath("(//table)["+i+"]//th")).size();
+		System.out.println("The total column in the table "+i+" is - "+table1colcount);
+		System.out.println("-------------------------------------------------");
+		//int table2count=driver.findElements(By.xpath("(//table)[2]//tr")).size();
+		//System.out.println("The total rows in the table in table 1 is - "+table2count);
+		//get the column count
+		//System.out.println("-------------------------------------------------");
+		//int table2colcount =driver.findElements(By.xpath("(//table)[2]//th")).size();
+		//System.out.println("The total column in the table 1 is - "+table2colcount);
+		}
+		driver.close();		
+	}
+}
