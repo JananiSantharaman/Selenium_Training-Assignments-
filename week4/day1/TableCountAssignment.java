673122810a4cd315304c44d9a82e1c90d0262695
JananiSantharaman <103834158+JananiSantharaman@users.noreply.github.com>
2022-05-09 18:46:52 +0530

    Update TableCountAssignment.java

--- week4/day1/TableCountAssignment.java
+++ week4/day1/TableCountAssignment.java
@@ -2,11 +2,9 @@ package week4.day1;
 
 import java.time.Duration;
 import java.util.List;
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class TableCountAssignment {
